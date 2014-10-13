7bf023e7f66c109af23984e9d8c5b98bd7920332
martinlundquist <martin.lundquist@hotmail.com>
2014-10-13 13:45:14 +0200

    skapat udpclient

---
+++ src/UdpClient.java
@@ -0,0 +1,5 @@
+/**
+ * Created by martinlundquist on 2014-10-13.
+ */
+public class UdpClient {
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,2 @@
 package com.company;
 
-public class Main {
-
-    public static void main(String[] args) {
-	//yo wazzuupp
-     System.out.println("yoman");
-    }
-
-    public int kompany(int kalas){
-        int jamon = Integer.parseInt("ganja");
-        return jamon;
-    }
-}
