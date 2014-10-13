827e2fea3620b602936dd06ae5771943d6c8f288
martinlundquist <martin.lundquist@hotmail.com>
2014-10-13 14:32:37 +0200

    Skapat allt mannen, en UDPclient..

--- src/UdpClient.java
+++ src/UdpClient.java
@@ -1,5 +1,25 @@
 /**
  * Created by martinlundquist on 2014-10-13.
  */
+import java.io.*;
+import java.net.*;
+
 public class UdpClient {
+    public static void main(String args[]) throws Exception{
+        BufferedReader userChoiseOfNameServer =
+                new BufferedReader(new InputStreamReader(System.in));
+        DatagramSocket clientSocket = new DatagramSocket();
+        InetAddress nameServerIP = InetAddress.getByName("itch.umu.se");
+        byte[] sendConnectRequest = new byte[1024];
+        byte[] receiveAnswer = new byte[1024];
+        String sentence =userChoiseOfNameServer.readLine();
+        sendConnectRequest = sentence.getBytes();
+        DatagramPacket reqPacket = new DatagramPacket(sendConnectRequest, sendConnectRequest.length, nameServerIP, 1337);
+        clientSocket.send(reqPacket);
+        DatagramPacket receivePacket = new DatagramPacket(receiveAnswer, receiveAnswer.length);
+        clientSocket.receive(receivePacket);
+        String fromServer = new String(receivePacket.getData());
+        System.out.println("From server: " + fromServer);
+        clientSocket.close();
+    }
 }
